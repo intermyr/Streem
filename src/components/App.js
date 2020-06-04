18a06843bc6a22a54135ed5e4d4d6035693fb77e
Alexander Muratidi <intermyr@gmail.com>
2020-06-04 21:41:51 +0300

    Create basic components

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { BrowserRoute, Route } from "react-router-dom";
+
+export const App = () => {
+  return <div>App</div>;
+};
---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const StreamCreate = () => {
+  return <div>StreamCreate</div>;
+};
