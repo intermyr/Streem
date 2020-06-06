dde70a0e44611f172b9f14e83e57b0baa4b15ac0
Alexander Muratidi <intermyr@gmail.com>
2020-06-06 19:52:23 +0300

    Implemented google auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GoogleAuth } from "./GoogleAuth";
 
 export const Header = () => {
   return (
     <div className="ui secondary pointing menu">
-        <Link to="/" className="item">
-          Streem
-        </Link>
+      <Link to="/" className="item">
+        Streem
+      </Link>
       <div className="right menu">
-        <Link to="/" className='item'>
+        <Link to="/" className="item">
           Stream List
         </Link>
+      </div>
+      <div className="">
+        <GoogleAuth />
       </div>
     </div>
   );
